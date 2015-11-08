cf6530fe36e9d1691038f3abdec63a04382784a7
mehrik <sobreo.m@gmail.com>
2015-11-07 23:15:20 -0800

    Emails deleteable from success.html. Email-validation box changes colors. Added email_database.sql

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
-import re
 from datetime import datetime
+import re
 app = Flask(__name__)
 app.secret_key = 'maricIsAwesome'
 mysql = MySQLConnector('emaildb')
@@ -9,22 +9,31 @@ EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 
 @app.route('/')
 def index():
-	print '********** STARTED UP **********'
 	return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
 	email = request.form['email']
-
 	if not EMAIL_REGEX.match(email) or len(email) < 1:
 		flash('Email is not valid!')
 		return redirect('/')
-
+	# Inserting email address into the database
 	query = "INSERT INTO emails (email_address, created_at)\
-			VALUES ('{}', NOW())".format(request.form['email'])
-
+			VALUES ('{}', NOW())".format(email)
 	mysql.run_mysql_query(query)
 	emailsdb = mysql.fetch('SELECT * FROM emails')
-	return render_template('success.html', emailsdb=emailsdb, your_email=email)
-	
+	return render_template('success.html', emailsdb=emailsdb, \
+		your_email=email, message='insert')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+	email_id = request.form['delete']
+	deleted_email = request.form['deleted_email']
+	# Deleting email address from the database
+	query = "DELETE FROM `emaildb`.`emails` WHERE `id`='{}'".format(email_id)
+	mysql.run_mysql_query(query)
+	emailsdb = mysql.fetch('SELECT * FROM emails')
+	return render_template('success.html', emailsdb=emailsdb, \
+		deleted_email=deleted_email, message='delete')
+
 app.run(debug=True)
